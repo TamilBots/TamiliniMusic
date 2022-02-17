89cbd89d8b97c617e480d85b87057968682eaf4c
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2022-02-17 07:19:26 -0800

    Update start.py

--- Tamilini/Inline/start.py
+++ Tamilini/Inline/start.py
@@ -62,7 +62,7 @@ def start_pannel():
         buttons = [
             [
                 InlineKeyboardButton(
-                    text="🗂 Helper Commands Menu", callback_data="shikhar"
+                    text="🗂 Helper Commands Menu", callback_data="TamilBots"
                 ),
             ],
             [
