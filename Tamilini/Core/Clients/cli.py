77e329b6b42101569874c0bb15e28bad9bb108c9
KuTTyVirgiN <101309338+KuTTyVirgiN@users.noreply.github.com>
2022-04-27 07:12:20 +0530

    Update cli.py

--- Tamilini/Core/Clients/cli.py
+++ Tamilini/Core/Clients/cli.py
@@ -2,7 +2,7 @@ from config import API_HASH, API_ID, BOT_TOKEN, STRING
 from pyrogram import Client
 
 app = Client(
-    "TamiliniMusic",
+    "KATARAMUSIC",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
